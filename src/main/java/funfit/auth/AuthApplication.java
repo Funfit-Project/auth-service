f1c5cc4474a560a8ae1cf7a8cb12da823b8531e1
olsohee <dlthgml0108@daum.net>
2024-06-13 17:54:16 +0900

    feat: spring cloud를 통한 라우팅 설정

--- src/main/java/funfit/auth/AuthApplication.java
+++ src/main/java/funfit/auth/AuthApplication.java
@@ -2,11 +2,13 @@ package funfit.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	public static void main(String[] args) {
