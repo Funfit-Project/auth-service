d7a9fdb7db92f72cf38415e07344c433488c528e
olsohee <dlthgml0108@daum.net>
2024-06-20 17:09:52 +0900

    fix: RabbitMQ Exchange, Routing Key 설정 변경

--- src/main/java/funfit/auth/rabbitMq/service/RabbitMqService.java
+++ src/main/java/funfit/auth/rabbitMq/service/RabbitMqService.java
@@ -4,6 +4,7 @@ import funfit.auth.rabbitMq.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,20 +14,23 @@ public class RabbitMqService {
 
     private final RabbitTemplate rabbitTemplate;
 
-    /**
-     * 회원 정보 변경 시 데이터 동기화를 위해 메시지 발행
-     */
-    public void publishEditedUserEmail(String email) {
-        log.info("RabbitMQ | publish message, queue name = edited_user_email, message = {}", email);
-        rabbitTemplate.convertAndSend("edited_user_email_for_pt", email);
-        rabbitTemplate.convertAndSend("edited_user_email_for_community", email);
-    }
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
 
     /**
      * PT 회원 회원가입 시 메시지 발행
      */
     public void publishCreateNewMember(CreateNewMemberPubDto createNewMemberPubDto) {
         log.info("RabbitMQ | publish message, queue name = create_new_member, message = {}", createNewMemberPubDto.toString());
-        rabbitTemplate.convertAndSend("create_new_member", createNewMemberPubDto);
+        rabbitTemplate.convertAndSend(exchange, "create_new_member", createNewMemberPubDto);
+    }
+
+    /**
+     * 회원 정보 변경 시 데이터 동기화를 위해 메시지 발행
+     */
+    public void publishEditedUserEmail(String email) {
+        log.info("RabbitMQ | publish message, queue name = edited_user_email, message = {}", email);
+        rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_pt", email);
+        rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_community", email);
     }
 }
