da18552f85456f0653820a30eb99559308c8697a
olsohee <dlthgml0108@daum.net>
2024-08-20 18:59:03 +0900

    refactor: mq 메시지 발행 비동기 처리 + retry 추가

--- src/main/java/funfit/auth/AuthApplication.java
+++ src/main/java/funfit/auth/AuthApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAsync
+@EnableRetry
 public class AuthApplication {
 
 	public static void main(String[] args) {
--- src/main/java/funfit/auth/rabbitMq/service/RabbitMqService.java
+++ src/main/java/funfit/auth/rabbitMq/service/RabbitMqService.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -20,17 +23,31 @@ public class RabbitMqService {
     /**
      * PT 회원 회원가입 시 메시지 발행
      */
+    @Async
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000, multiplier = 1.5, maxDelay = 5000))
     public void publishCreateNewMember(CreateNewMemberPubDto createNewMemberPubDto) {
         log.info("RabbitMQ | publish message, queue name = create_new_member, message = {}", createNewMemberPubDto.toString());
-        rabbitTemplate.convertAndSend(exchange, "create_new_member", createNewMemberPubDto);
+        try {
+            rabbitTemplate.convertAndSend(exchange, "create_new_member", createNewMemberPubDto);
+        } catch (Exception e) {
+            log.error("MQ 메시지 발행 실패, retry 시도");
+            throw e;
+        }
     }
 
     /**
      * 회원 정보 변경 시 데이터 동기화를 위해 메시지 발행
      */
+    @Async
+    @Retryable(value = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000, multiplier = 1.5, maxDelay = 5000))
     public void publishEditedUserEmail(String email) {
         log.info("RabbitMQ | publish message, queue name = edited_user_email, message = {}", email);
-        rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_pt", email);
-        rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_community", email);
+        try {
+            rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_pt", email);
+            rabbitTemplate.convertAndSend(exchange, "edited_user_email_for_community", email);
+        } catch (Exception e) {
+            log.error("MQ 메시지 발행 실패, retry 시도");
+            throw e;
+        }
     }
 }
